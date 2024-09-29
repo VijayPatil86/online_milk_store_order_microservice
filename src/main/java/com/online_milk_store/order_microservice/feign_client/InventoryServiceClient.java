2bd58df301faec31308ff9331739f59b64d82f05
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-29 18:42:12 +0530

    removed method to check inventory status for selected products

--- src/main/java/com/online_milk_store/order_microservice/feign_client/InventoryServiceClient.java
+++ src/main/java/com/online_milk_store/order_microservice/feign_client/InventoryServiceClient.java
@@ -1,14 +1,7 @@
 package com.online_milk_store.order_microservice.feign_client;
 
-import java.util.Map;
-
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "inventory-service", path = "/inventory-service/inventory")
 public interface InventoryServiceClient {
-	@GetMapping("/milk-brand")
-	ResponseEntity<Map<String, Object>> checkInventoryMilkBrands(@RequestParam Map<String, String> queryParams);
 }
