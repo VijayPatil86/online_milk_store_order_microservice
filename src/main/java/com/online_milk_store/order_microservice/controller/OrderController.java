136981848dc301f39a032c4b9e0150462ece8467
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-06 19:09:17 +0530

    updated base request mapping url added code to process order

--- src/main/java/com/online_milk_store/order_microservice/controller/OrderController.java
+++ src/main/java/com/online_milk_store/order_microservice/controller/OrderController.java
@@ -3,18 +3,36 @@ package com.online_milk_store.order_microservice.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.online_milk_store.order_microservice.bean.Order;
 import com.online_milk_store.order_microservice.feign_client.InventoryServiceClient;
+import com.online_milk_store.order_microservice.service.OrderService;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/order-service")
+@RequestMapping("/order-service/order")
 public class OrderController {
 	static final private Logger LOGGER = LogManager.getLogger(OrderController.class);
 
 	@Autowired
 	private InventoryServiceClient inventoryServiceClient;
+
+	@Autowired
+	private OrderService orderService;
+
+	@PostMapping
+	public ResponseEntity<String> processOrder(@RequestBody Order order) {
+		LOGGER.debug("OrderController.processOrder() --- START");
+		LOGGER.info("OrderController.processOrder() --- order: " + order);	// Order [productIdQty=21=2&22=4, paymentDetails=PaymentDetails [paymentMethod=UPIPaymentMethod [upiID=acc@hdfc, paymentDescription=buy milk piuch]]]
+		orderService.processOrder(order);
+		LOGGER.debug("OrderController.processOrder() --- END");
+		return new ResponseEntity<>("Order processed...", HttpStatus.OK);
+	}
 }
