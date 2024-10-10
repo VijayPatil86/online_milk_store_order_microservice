cd3431479fe031ebc404dea04aa1ceb9ca09a248
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-10 12:02:50 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/order_microservice/repository/UPIDetailRepository.java
@@ -0,0 +1,11 @@
+package com.online_milk_store.order_microservice.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.online_milk_store.order_microservice.entity.UPIDetailsEntity;
+
+public interface UPIDetailRepository extends JpaRepository<UPIDetailsEntity, Integer> {
+	Optional<UPIDetailsEntity> findByUpiId(String upiId);
+}
