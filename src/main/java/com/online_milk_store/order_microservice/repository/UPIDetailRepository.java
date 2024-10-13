979ce47da57623bec03e5b6d858e7023974f5891
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-13 11:50:54 +0530

    removed method findByUpiId()

--- src/main/java/com/online_milk_store/order_microservice/repository/UPIDetailRepository.java
+++ src/main/java/com/online_milk_store/order_microservice/repository/UPIDetailRepository.java
@@ -1,11 +1,9 @@
 package com.online_milk_store.order_microservice.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.online_milk_store.order_microservice.entity.UPIDetailsEntity;
 
 public interface UPIDetailRepository extends JpaRepository<UPIDetailsEntity, Integer> {
-	Optional<UPIDetailsEntity> findByUpiId(String upiId);
+	//Optional<UPIDetailsEntity> findByUpiId(String upiId);
 }
