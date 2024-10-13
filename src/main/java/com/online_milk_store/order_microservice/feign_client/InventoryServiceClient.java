df2b1e2edac1b1cae61cd8fc8105f6b6b2014570
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-13 19:11:51 +0530

    removed

--- src/main/java/com/online_milk_store/order_microservice/feign_client/InventoryServiceClient.java
+++
@@ -1,7 +0,0 @@
-package com.online_milk_store.order_microservice.feign_client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "inventory-service", path = "/inventory-service/inventory")
-public interface InventoryServiceClient {
-}
