8d5270dcc3cfe6dd06954083690a2f5c76924b19
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-13 19:15:28 +0530

    removed

--- src/main/java/com/online_milk_store/order_microservice/repository/UPIDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.online_milk_store.order_microservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.online_milk_store.order_microservice.entity.UPIDetailsEntity;
-
-public interface UPIDetailRepository extends JpaRepository<UPIDetailsEntity, Integer> {
-	//Optional<UPIDetailsEntity> findByUpiId(String upiId);
-}
