97a9369c965277d5a301ee54b59de4590aa2e407
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-16 15:37:36 +0530

    called Payment service

--- src/main/java/com/online_milk_store/order_microservice/controller/OrderController.java
+++ src/main/java/com/online_milk_store/order_microservice/controller/OrderController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.online_milk_store.order_microservice.bean.OrderBean;
+import com.online_milk_store.order_microservice.bean.UPIPaymentTransactionBean;
+import com.online_milk_store.order_microservice.feign_client.PaymentServiceClient;
 import com.online_milk_store.order_microservice.service.OrderService;
 
 @CrossOrigin
@@ -23,11 +25,15 @@ public class OrderController {
 	@Autowired
 	private OrderService orderService;
 
+	@Autowired
+	private PaymentServiceClient paymentServiceClient;
+
 	@PostMapping
 	public ResponseEntity<String> processOrder(@RequestBody OrderBean orderBean) {
 		LOGGER.debug("OrderController.processOrder() --- START");
 		LOGGER.info("OrderController.processOrder() --- orderBean: " + orderBean);	// Order [productIdQty=21=2&22=4, paymentDetails=PaymentDetails [paymentMethod=UPIPaymentMethod [upiID=acc@hdfc, paymentDescription=buy milk piuch]]]
-		orderService.processOrder(orderBean);
+		UPIPaymentTransactionBean upiPaymentTransactionBean = orderService.processOrder(orderBean);
+		ResponseEntity<Void> responseEntity = paymentServiceClient.processUPIPayment(upiPaymentTransactionBean);
 		LOGGER.debug("OrderController.processOrder() --- END");
 		return new ResponseEntity<>("Order processed...", HttpStatus.OK);
 	}
